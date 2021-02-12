64bdba0737bddca4027ad3d33c0c17fa3fea5d29
c4pi <71392133+capisms@users.noreply.github.com>
2021-02-12 16:26:44 -0300

    Updated 02/12/2021
    Rules command
    Verify command 
    
    ADD

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
+// ALL THIS SHIT HAS BEEN DEVELOPED BY CAPI .
+// I dont need help of nobody ! 227Glocks#7926 for support :)
+
+// Const
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { MessageEmbed } = require('discord.js');
+const { Client } = require('discord.js');
 
+// Rich presence
 
 function presence(){
    client.user.setPresence({
       status: "online",
       activity: {
-         name: "Developed by Capi",
+         name: "Developed by capi!",
          type: "PLAYING"
       }
    });
 }
 
-console.log("Capi bot discord / Loading..")
+// Console.logs , you can deleted this. Is shit xD
+console.log("Bot discord / Loading..")
 
 client.on('ready', () => {
-    console.log("BOT STARTED / Discord server Started. ")
-    presence()
+  console.log(`-------------------------------\nWelcome to the bot \nCreator / Developer : 227Glocks#7926\nConnected to ${client.user.tag}!\n-------------------------------`);
+  presence()
 }); 
 
 console.log("------------")
-console.log("BOT STARTED /  Loading..")
-console.log("------------")
-console.log("BOT STARTED /  Loading..")
-console.log("------------")
-console.log("BOT STARTED /  Loading..")
-console.log("------------")
-console.log("BOT STARTED /  Loading..")
-console.log("------------")
 console.log("BOT STARTED / Loading..")
-console.log("------------")
-console.log("BOT STARTED / Loading..")
-console.log("------------")
-console.log("BOT STARTED /  Loading..")
 console.log("------------")
 
- 
+console.log("Dm 227Glocks#7926 on Discord to get more source codes :) This bot has been developed by me without help of nobody ! ")
+console.log("------------")
+
+
+// Shit commands : !nitro, !stresser , !comandos, *register, *download, AND !verify 
+ client.on("message", (message) => {
+
+   if(message.content.startsWith("!source-code")) {
+    message.channel.send("Here is the source code ;) https://github.com/capisms/Discord-Bot-JS-");
+  }
+    
+   if(message.content.startsWith("!verify")) {
+     message.channel.send("`🤡 You get verified !`");
+   }
+
+   if(message.content.startsWith("!asd")) {
+    message.channel.send("[@here]`🤡 - Para verificarse hay que escribir !verify en el canal #verify - 🤡`[@here]")
+  }
+ });
+
+
+//Avatar Code // Command : dev-capi-avatar
+
  client.on("message", msg => {
           let user = msg.mentions.users.first(); 
           if(!user) user = msg.author; 
           const embed = new Discord.MessageEmbed() 
-          .setTitle("Here is your poor photo") 
+          .setTitle("🤡 **Your photo**") 
           .setImage(user.displayAvatarURL()) 
           .setTimestamp() 
           if(msg.content.startsWith("dev-capi-avatar")){ 
@@ -49,14 +66,65 @@ console.log("------------")
           } 
         });
 
+ // Rules of the server // command !embed
+        
+        client.on("message", msg => {
+          let user = msg.mentions.users.first(); 
+          if(!user) user = msg.author; 
+          const embed = new Discord.MessageEmbed() 
+          .setTitle("**Reglas del discord**")
+          .setFooter("Discord Bot By Capi ! I dont need help of nobody!")
+          .setThumbnail('INSERT PHOTO')
+          .addFields(
+            {
+              name: 'Regla 1',
+              value: '• RULE',
+              inline: false,
+            },
+
+            {
+              name: 'Regla 2',
+              value: '• RULE',
+              inline: false,
+            },
+
+            {
+              name: 'Regla 3',
+              value: '• RULE.',
+              inline: false,
+            },
+
+            {
+              name: 'Regla 4',
+              value: '• RULE',
+              inline: false,
+            },
+
+            {
+              name: 'Regla 5',
+              value: '• RULE',
+              inline: false,
+            },
+          )  
+          .setTimestamp() 
+          if(msg.content.startsWith("!embed")){ 
+            msg.reply(embed) 
+          } 
+        });
+
+// Clear code // Command : dev-capi-clear 10 , you can change the bulkDelete        
+
  client.on("message", message => {
   if(message.content === "dev-capi-clear 10") {
     message.channel.bulkDelete(10)
-    .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+    .then(messages => console.log(`Deleted ${messages.size} messages`))
     .catch(console.error);
- message.channel.send("Cleared");
+ message.channel.send("Chat limpio");
   }
 })
+
+
+// Ban code // Command : !ban {user}
 
  client.on("message", async message => {
    prefix = '!'
@@ -81,11 +149,11 @@ console.log("------------")
        })
        .catch(err => {
   
- message.reply('\  **No puede banear a este miembro**');
+ message.reply('\ **Este usuario no puede ser baneado (Administrador)**');
    console.log;
     });
 } else{
-    message.reply("\  **Este usuario no está en este servidor!**")
+    message.reply("\ **Este usuario no está en este servidor!**")
 }
 } else {
 message.reply('\**Usuario?**')
@@ -94,4 +162,79 @@ message.reply('\**Usuario?**')
 })
 
 
-client.login("Insert ur token :)");
+//Verify code // Command: !verify 
+
+client.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.channel.id === 'channel id')
+        await message.delete();
+    if(message.content.toLowerCase() === '!verify' && message.channel.id === 'channel id')
+    {   
+        await message.delete().catch(err => console.log(err));
+        const role = message.guild.roles.cache.get('role id');
+        if(role) {
+            try {
+                await message.member.roles.add(role);
+                console.log("Role agregado!");
+            }
+            catch(err) {
+                console.log(err);
+            }
+        }
+    }
+});
+
+// Template of rules-embed // You can deleted if u want
+
+        client.on("message", msg => {
+          let user = msg.mentions.users.first(); 
+          if(!user) user = msg.author; 
+          const embed = new Discord.MessageEmbed() 
+
+          .setTitle("**Template**")
+          .setFooter("Discord Embed Message Template by Capi !")
+          .setThumbnail('insert photo if you want')
+          .addFields(
+            {
+              name: 'Template',
+              value: '• Template',
+              inline: false,
+            },
+
+            {
+              name: 'Template',
+              value: '• Template',
+              inline: false,
+            },
+
+            {
+              name: 'Template',
+              value: '• Template',
+              inline: false,
+            },
+
+            {
+              name: 'Template',
+              value: '• Template',
+              inline: false,
+            },
+
+            {
+              name: 'Regla 5',
+              value: '• Template',
+              inline: false,
+            },
+          )  
+          .setTimestamp() 
+          if(msg.content.startsWith("!template")){ 
+            msg.reply(embed) 
+          } 
+        });
+
+client.on('guildMemberAdd', member => {
+    console.log(member.user.tag);
+});
+
+// Token login !
+
+client.login("insert your token :)");
